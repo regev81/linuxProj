0a062155370b5813f1249cec25cd8d9276feb830
Regev <regev81@gmail.com>
2021-01-30 19:22:59 +0200

    running with 1 subkey

--- main.c
+++ main.c
@@ -1,61 +1,50 @@
 #include <stdio.h>
 #include <string.h>
 #include "functions.h"
-#define FILE "file.txt"
+#define PLAINTEXTFILE "plaintextfile.txt"
+#define CYPHERFILE "cypherfile.txt"
 //https://en.wikipedia.org/wiki/Data_Encryption_Standard
 void testMainRoy();
 
 int main(int argc, char* argv[]) 
 {
 	testMainRoy();
-	
 	return 1;
 }
 
 void testMainRoy()
 {
-	int blocksAmount,i;
-	int charCount = findFileSize(FILE);
-	char* charArr = fileToCharArray(FILE, charCount);
+	int blocksAmount, i, j;
+	int rounds = 16;
+	int charCount = findFileSize(PLAINTEXTFILE);
+	char* charArr = fileToCharArray(PLAINTEXTFILE, charCount);
+	int** encriptionBlocksResult;
+	int* encriptionResult;
 
 	//create array of all the chars blocks from the file content
 	char** charsBlocksArr = CharArrToCharBlocks(charArr, charCount, &blocksAmount);
-	//printf("All blocks[CHARS]:\n");
-	//printCharsBlock(charsBlocksArr, blocksAmount);
+
+	//create encriptionBlocksResult arr
+	encriptionBlocksResult = (int**)malloc(blocksAmount * sizeof(int*));
 
 	//iterate each chars block, convert it to bits block and encript 
 	printf("\nStart Encription:\n----------------------------------------------\n");
 	for (i = 0; i < blocksAmount; i++)
 	{
-		printf("Block #%d:\n",i);
+		printf("Block #%d:\n--------------------------------\n\n",i);
 		printCharBlock(charsBlocksArr[i]);
 		
 		//convert current char block to bits block
 		int* bitsBlock = charArrToBitsArr(charsBlocksArr[i], CHARSINBLOCK);
-		printf("bits block:\n", i);
+		printf("bits block:\n");
 		printBitsArr(bitsBlock, BITSINBLOCK);
 
 		//perform initial permutation
-		int* bitsBlockAfterInitialPer = initialPermutation(bitsBlock, CHARSINBLOCK);
-		printf("InitialPer bits:\n", i);
+		int* bitsBlockAfterInitialPer = initialPermutation(bitsBlock);
+		printf("InitialPer bits:\n");
 		printBitsArr(bitsBlockAfterInitialPer, BITSINBLOCK);
-
-		//perform block encription
-		////////////////////////////////////////////////////////////
-
-		//left half block
-		HalfBlockSide left = LEFT;
-		int* leftHalfBlock = divideBlock(bitsBlockAfterInitialPer, left);
-		printf("LEFT side:\n");
-		printBitsArr(leftHalfBlock, BITSINBLOCK / 2);
-
-		//right half block
-		//HalfBlockSide right = RIGHT;
-		//int* rightHalfBlock = divideBlock(bitsBlockAfterInitialPer, right);
-		//printf("RIGHT side:\n");
-		//printBitsArr(rightHalfBlock, BITSINBLOCK / 2);
-
-		//
+		
+		/////////////////////////
 		int subkeytest[48] =
 		{
 			1, 0, 1, 0, 1, 0, 1, 0,
@@ -66,22 +55,77 @@ void testMainRoy()
 			1, 0, 0, 1, 0, 0, 0, 1
 
 		};
-		feistelFunction(leftHalfBlock, subkeytest);
+		/////////////////////////
+
+		//right half block
+		HalfBlockSide right = RIGHT;
+		int* rightHalfBlock = getHalfBlock(bitsBlockAfterInitialPer, right);
+		printf("RIGHT side:\n");
+		printBitsArr(rightHalfBlock, BITSINBLOCK / 2);
+
+		//left half block
+		HalfBlockSide left = LEFT;
+		int* leftHalfBlock = getHalfBlock(bitsBlockAfterInitialPer, left);
+		printf("LEFT side:\n");
+		printBitsArr(leftHalfBlock, BITSINBLOCK / 2);
+
+		//start encription rounds
+		for (j = 0; j < rounds; j++)
+		{
+			printf("\nRound #%d:\n---------------\n", j);
+			int* leftHalfBlockOutput;
+			int* rightHalfBlockOutput;
+			prformRound(leftHalfBlock, rightHalfBlock, &leftHalfBlockOutput, &rightHalfBlockOutput, subkeytest);
+			leftHalfBlock = leftHalfBlockOutput;
+			rightHalfBlock = rightHalfBlockOutput;
+
+			printf("Round #%d LEFT output:\n", j);
+			printBitsArr(leftHalfBlockOutput, BITSINBLOCK / 2);
+			printf("Round #%d RIGHT output:\n", j);
+			printBitsArr(rightHalfBlockOutput, BITSINBLOCK / 2);
+		}
+		
+		//join the 2 half blocks (left and right)
+		int* joinedBlock = joinHalfblocks(rightHalfBlock, leftHalfBlock);
+
+		//perform the final permutation
+		encriptionBlocksResult[i] = finalPermutation(joinedBlock);
+		printf("Block encription output:\n");
+		printBitsArr(encriptionBlocksResult[i], BITSINBLOCK);
 
 
-		//free allocated memory
+
+		//free memory
 		free(bitsBlock);
 		free(bitsBlockAfterInitialPer);
 		free(leftHalfBlock);
+		free(rightHalfBlock);
+		free(joinedBlock);
 		printf("\n");
 	}
 
-	//free allocated memory
+	//flattern the blocks to single arr of bits
+	encriptionResult = (int*)malloc((blocksAmount*BITSINBLOCK) * sizeof(int));
+	for (i = 0; i < (blocksAmount * BITSINBLOCK);i++)
+	{
+		encriptionResult[i] = encriptionBlocksResult[i / BITSINBLOCK][i % BITSINBLOCK];
+	}
+
+	//print cypher result
+	printf("cypher result:\n");
+	printBitsArr(encriptionResult, blocksAmount * BITSINBLOCK);
+
+	//save result to file
+	saveEncriptionResult(encriptionResult, blocksAmount * BITSINBLOCK, CYPHERFILE);
+
+	//free memory
 	free(charArr);
+	free(encriptionResult);
 	for (i = 0; i < blocksAmount; i++)
 	{
 		free(charsBlocksArr[i]);
+		free(encriptionBlocksResult[i]);
 	}
 	free(charsBlocksArr);
-
+	free(encriptionBlocksResult);
 }
